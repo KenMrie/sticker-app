c597af73866c273fc0502aa4d1691b9a74e20717
KenMrie <kendymarierodriguezinoa@gmail.com>
2023-11-16 15:26:55 -0400

    pruebax3

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,31 +1,31 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useNavigate } from "react-router-dom";
 
-useNavigate
+export const NavbarMenu = () => {
+  const navigate = useNavigate();
 
-export class NavbarMenu extends Component {
-  static propTypes = {}
-
-  render() {
-    return (
-      <>
+  const goToLink = (enlace) => {
+    navigate(`/${enlace}`)
+  }
+  return (
+    <>
       <Navbar bg="success" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="/">ステッカー</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="https://sticker-app-eta.vercel.app/">Home</Nav.Link>
-            <Nav.Link href="https://sticker-app-eta.vercel.app/stickers">Available stickers</Nav.Link>
-            <Nav.Link href="https://sticker-app-eta.vercel.app/aboutUs">About us</Nav.Link>
+            <Nav.Link onClick={() => ("/")}>Home</Nav.Link>
+            <Nav.Link onClick={() => ("/stickers")}>
+              Available stickers
+            </Nav.Link>
+            <Nav.Link onClick={() => ("/aboutUs")}>
+              About us
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
     </>
-    )
- }
-}
-
-export default NavbarMenu
+  );
+};
